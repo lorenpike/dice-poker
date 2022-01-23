78f4b4e59a829e7318c5da112a2a4c8d931aaf3b
lorenpike <noahceverett@gmail.com>
2022-01-23 11:03:17 -0700

    Tests for horizontal sequences

--- dice_poker/board.py
+++ dice_poker/board.py
@@ -1,11 +1,29 @@
+from enum import Enum
+from typing import List, NamedTuple, Tuple
+
 from .dice_patterns import * 
 
-class FreeSpace(Pattern):
-    @staticmethod
-    def check(roll: List[int]) -> bool:
-        return True
+
+class FreeSpace:
+    pass
+
+class Color(Enum):
+    RED = 0
+    BLUE = 1
+
+class Spot(NamedTuple):
+    row: int
+    col: int
+
+class Sequence(NamedTuple):
+    color: Color
+    start: Spot
+    end: Spot
 
 class Board:
+    BOARD_SIZE = 9
+    FREE_SPACE = Spot(4,4)
+
     BOARD = [
         [Straight(), Double(6,6), Double(5,6), Double(4,6), Pair(2), Double(3,6), Double(2,6), Double(1,6), Straight()],
         [Pair(6), FullHouse(), Double(1,5), Double(2,5), Double(3,5),Double(4,5), Double(5,5), FullHouse(), Pair(4)],
@@ -19,6 +37,82 @@ class Board:
     ]
 
     def __init__(self) -> None:
-        pass
+        self.create_new_board()
 
-    
+    def create_new_board(self):
+        self.chip_placements = [[None for _ in range(Board.BOARD_SIZE)] for _ in range(Board.BOARD_SIZE)]
+
+    def place_blue_chip(self, row: int, col: int):
+        self.place_chip(Color.BLUE, row, col)
+
+    def place_red_chip(self, row: int, col: int):
+        self.place_chip(Color.RED, row, col)
+
+    @staticmethod
+    def is_on_board(row: int, col: int):
+        if row > 8 or row < 0 or col > 8 or col < 0:
+            raise ValueError("Placement is out of range")
+
+    def check_valid_placement(self, row: int, col: int):
+        Board.is_on_board(row, col)
+        if row == 4 and col ==4:
+            raise ValueError("Cannot place chip on Free Space")
+        if self.chip_placements[row][col] is not None:
+            raise ValueError("Spot is already claimed")
+        
+    def place_chip(self, color: Color, row: int, col: int):
+        self.check_valid_placement(row, col)
+        self.chip_placements[row][col] = color
+
+    def get_spot(self, spot: Spot):
+        Board.is_on_board(spot.row, spot.col)
+        return self.chip_placements[spot.row][spot.col]
+
+    def find_contigious_regions(self, strip: List[Spot]):
+        sections = []
+        first_spot = strip[0]
+        tracking = self.get_spot(first_spot)
+        start = 0
+        for i, spot in enumerate(strip):
+            if self.get_spot(spot) != tracking and spot != Board.FREE_SPACE:
+                sections.append((tracking, (start, i-1)))
+                start = i
+                tracking = self.get_spot(spot)
+                if strip[i-1] == Board.FREE_SPACE:
+                    start -= 1
+        sections.append((tracking, (start, Board.BOARD_SIZE - 1)))
+        return sections
+
+    def find_horizontal_sequence_in_row(self, row: int):
+        row_spots = []
+        for col in range(Board.BOARD_SIZE):
+            row_spots.append(Spot(row, col))
+        sections = self.find_contigious_regions(row_spots)
+        sequences = []
+        for sect in sections:
+            if sect[0] in {Color.BLUE, Color.RED} and sect[1][1] - sect[1][0] + 1 >= 5:
+                sequences.append(sect)
+        return sequences
+
+    def find_horizontal_sequences(self):
+        sequences = set()
+        for col in range(Board.BOARD_SIZE):
+            seqs = self.find_horizontal_sequence_in_row(col)
+            for s in seqs:
+                sequences.add(Sequence(s[0], Spot(col, s[1][0]), Spot(col, s[1][1])))
+        return sequences
+
+
+    def __str__(self) -> str:
+        return "\n".join([" ".join(i) for i in self.chip_placements])
+
+def find_longest_sequence(nums: List[int]):
+    nums.sort()
+    longest_seq = seq = []
+    for i, el in enumerate(nums):
+        seq.append(el)
+        if len(nums) == i + 1 or el + 1 != nums[i+1]:
+            if len(seq) >= len(longest_seq):
+                longest_seq = seq
+            seq = []
+    return longest_seq
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,6 +1,127 @@
-from dice_poker.board import Board
+from gettext import find
+import pytest
+
+from dice_poker.board import Board, Color, Sequence, Spot, find_longest_sequence
 
 def test_board_size():
     assert len(Board.BOARD) == 9
     for row in Board.BOARD:
         assert len(row) == 9
+
+def test_no_chip_placements():
+    board = Board()
+    assert len(board.chip_placements) == 9
+    for row in board.chip_placements:
+        assert  len(row) == 9
+        for el in row:
+            assert el is None
+
+def test_place_a_blue_chip():
+    board = Board()
+    board.place_blue_chip(row=0, col=0)
+    assert board.chip_placements[0][0] == Color.BLUE
+
+def test_place_a_red_chip():
+    board = Board()
+    board.place_red_chip(row=0, col=0)
+    assert board.chip_placements[0][0] == Color.RED
+
+def test_place_blue_then_red():
+    board = Board()
+    board.place_blue_chip(row=0, col=0)
+    board.place_red_chip(row=0, col=1)
+    assert board.chip_placements[0][0] == Color.BLUE
+    assert board.chip_placements[0][1] == Color.RED
+
+def test_raise_error_putting_chip_on_same_spot():
+    board =  Board()
+    board.place_blue_chip(row=0, col=0)
+    with pytest.raises(ValueError):
+        board.place_red_chip(row=0, col=0)
+
+def test_cannot_place_chip_on_free_space():
+    board = Board()
+    with pytest.raises(ValueError):
+        board.place_red_chip(row=4, col=4)
+
+def test_get_spot():
+    board = Board()
+    board.place_blue_chip(0,0)
+    spot_1 = Spot(0,0)
+    spot_2 = Spot(0,1)
+    assert board.get_spot(spot_1) == Color.BLUE
+    assert board.get_spot(spot_2) is None
+
+def test_raise_error_when_outside_board():
+    board = Board()
+    with pytest.raises(ValueError):
+        board.place_blue_chip(row=9, col=1)
+    with pytest.raises(ValueError):
+        board.place_blue_chip(row=0, col=9)
+    with pytest.raises(ValueError):
+        board.place_blue_chip(row=-1, col=5)
+    with pytest.raises(ValueError):
+        board.place_blue_chip(row=0, col=-1)
+        
+def test_find_horizontal_sequence_in_row_0():
+    board = Board()
+    for i in range(5):
+        board.place_blue_chip(row=0, col=i+1)
+    
+    assert board.find_horizontal_sequence_in_row(row=0) == [(Color.BLUE, (1,5))]
+
+def test_find_horizontal_sequences():
+    board = Board()
+    for i in range(5):
+        board.place_blue_chip(row=0, col=i+1)
+    expected = {Sequence(Color.BLUE, Spot(0,1), Spot(0,5))}
+    assert board.find_horizontal_sequences() == expected
+
+def test_find_two_horizontal_sequences():
+    board = Board()
+    for i in range(1,6):
+        board.place_blue_chip(row=0, col=i)
+        if i != 4:
+            board.place_red_chip(row=4, col=i)
+
+    expected = {
+        Sequence(Color.BLUE, Spot(0,1), Spot(0,5)), 
+        Sequence(Color.RED, Spot(4,1), Spot(4,5))
+    }
+
+    assert board.find_horizontal_sequences() == expected
+
+def test_two_sequences_on_middle_row():
+    board = Board()
+    for i in range(4):
+        board.place_blue_chip(row=4, col=i)
+        board.place_red_chip(row=4, col=i+5)
+
+    expected = {
+        Sequence(Color.BLUE, Spot(4,0), Spot(4,4)),
+        Sequence(Color.RED, Spot(4,4), Spot(4,8))
+    }
+
+    assert board.find_horizontal_sequences() == expected
+
+def test_find_contiguious_sections_in_list_of_spots():
+    board = Board()
+    spots = []
+    for i in range(9):
+        spots.append(Spot(0, i))
+    for i in range(3):
+        board.place_blue_chip(0, i)
+        board.place_red_chip(0, i+6)
+    assert board.find_contigious_regions(spots) == [(Color.BLUE, (0,2)), (None, (3,5)), (Color.RED, (6,8))]
+
+# def test_find_vertical_sequence():
+#     board = Board()
+#     for i in range(3,9):
+#         board.place_red_chip(row=i, col=0)
+
+#     assert board.find_vertical_sequences() == {(Color.RED, (3,0), (8,0))}
+
+def test_longest_sequence():
+    assert find_longest_sequence([1,3,4,5,6,7]) == [3,4,5,6,7]
+    assert find_longest_sequence([1,2,3,4,5,7]) == [1,2,3,4,5]
+    assert find_longest_sequence([1,3,7,5,6]) == [5,6,7]
