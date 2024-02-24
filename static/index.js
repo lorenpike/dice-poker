8f4da015beafd4b801fe29ca0dca2ed2283e5636
lorenpike <noahe@metrized.com>
2024-02-24 13:34:10 -0800

    Made the homepage more functional and look nice

--- static/index.js
+++ static/index.js
@@ -2,3 +2,16 @@ document.getElementById('new-game').addEventListener('click', () => {
     console.log("New game");
     window.location.href = "/game/new";
 });
+
+document.getElementById('join-game').addEventListener('submit', (e) => {
+
+    console.log("Join game");
+    e.preventDefault();
+
+    const gameId = document.getElementById('game-id').value;
+    if (!gameId) {
+        alert("Please enter a game ID.");
+        return;
+    }
+    window.location.href = `/game/${gameId}`;
+});
